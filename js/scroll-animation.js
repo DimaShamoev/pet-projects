c52121b60baff87df598ec9acebd1f6919d1d4b0
Dima <153534443+DimaShamoev@users.noreply.github.com>
2024-07-25 01:14:54 +0400

    Update scroll-animation.js

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -33,43 +33,28 @@
 // };
 
 
-window.onload = function () {
-    let html = document.documentElement;
-    let canvas = document.querySelector(".anim-canvas");
-    let context = canvas.getContext('2d');
+function preloadImages(frameCnt, callback) {
+    let images = [];
+    let loadedImages = 0;
 
-    const curentFrame = index => (
-        `../../img/frames/frame_${index.toString().padStart(4, '0')}.png`
-    );
+    for (let i = 1; i <= frameCnt; i++) {
+        let img = new Image();
+        img.src = curentFrame(i);
+        img.onload = () => {
+            loadedImages++;
+            if (loadedImages === frameCnt) {
+                callback(images);
+            }
+        };
+        images.push(img);
+    }
+}
 
-    let frameCnt = 1135;
-    let img = new Image();
+const curentFrame = index => (
+    `../../img/frames/frame_${index.toString().padStart(4, '0')}.png`
+);
 
-    img.onload = function () {
-        canvas.height = img.height;
-        canvas.width = img.width;
-        context.drawImage(img, 0, 0);
-    };
+preloadImages(1135, (images) => {
+    // Animation code here
+});
 
-    img.src = curentFrame(1);
-
-    const updateImage = index => {
-        img.src = curentFrame(index);
-    };
-
-    let ticking = false;
-
-    window.addEventListener("scroll", () => {
-        if (!ticking) {
-            window.requestAnimationFrame(() => {
-                let scrollTop = html.scrollTop;
-                let maxScroll = html.scrollHeight - window.innerHeight;
-                let scrollFraction = scrollTop / maxScroll;
-                let frameIndex = Math.min(frameCnt - 1, Math.floor(scrollFraction * frameCnt));
-                updateImage(frameIndex + 1);
-                ticking = false;
-            });
-            ticking = true;
-        }
-    });
-};
